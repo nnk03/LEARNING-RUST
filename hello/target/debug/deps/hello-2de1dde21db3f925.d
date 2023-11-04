4a922746310f31bab812d418e1617f902abe64fc
nnk03 <neerajnjps03@gmail.com>
2023-11-04 13:16:21 +0530

    added hello

---
+++ hello/target/debug/deps/hello-2de1dde21db3f925.d
@@ -0,0 +1,5 @@
+/Users/neerajkrishnan/Desktop/PROGRAMMING LANGUAGES/RUST/LEARNING-RUST/hello/target/debug/deps/hello-2de1dde21db3f925: src/main.rs
+
+/Users/neerajkrishnan/Desktop/PROGRAMMING LANGUAGES/RUST/LEARNING-RUST/hello/target/debug/deps/hello-2de1dde21db3f925.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/neerajkrishnan/Desktop/PROGRAMMING\ LANGUAGES/RUST/LEARNING-RUST/hello/target/debug/hello: /Users/neerajkrishnan/Desktop/PROGRAMMING\ LANGUAGES/RUST/LEARNING-RUST/hello/src/main.rs
