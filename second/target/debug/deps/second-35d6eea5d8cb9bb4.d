1fbbb871ac19d16bc9d2b6b8f8fa53492638c971
nnk03 <neerajnjps03@gmail.com>
2023-12-02 12:32:48 +0530

    learning rust from documentation, skipped slices

---
+++ second/target/debug/deps/second-35d6eea5d8cb9bb4.d
@@ -0,0 +1,5 @@
+/Users/neerajkrishnan/Desktop/PROGRAMMING LANGUAGES/RUST/LEARNING-RUST/second/target/debug/deps/second-35d6eea5d8cb9bb4: src/main.rs
+
+/Users/neerajkrishnan/Desktop/PROGRAMMING LANGUAGES/RUST/LEARNING-RUST/second/target/debug/deps/second-35d6eea5d8cb9bb4.d: src/main.rs
+
+src/main.rs:
---
+++ second/target/debug/second.d
@@ -0,0 +1,1 @@
+/Users/neerajkrishnan/Desktop/PROGRAMMING\ LANGUAGES/RUST/LEARNING-RUST/second/target/debug/second: /Users/neerajkrishnan/Desktop/PROGRAMMING\ LANGUAGES/RUST/LEARNING-RUST/second/src/main.rs
