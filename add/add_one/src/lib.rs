b682e5f0892f692f4a99180b28f906076d7df76c
Neeraj Krishna N <neerajnjps03@gmail.com>
2024-11-30 14:41:06 +0530

    cargo workspaces

---
+++ add/add_one/src/lib.rs
@@ -0,0 +1,30 @@
+use rand;
+
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(3, add_one(2));
+    }
+}
+
+// pub fn add(left: u64, right: u64) -> u64 {
+//     left + right
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
